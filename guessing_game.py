df925058544bfe2fcbc25dd1f8bc2ed69ae4c6f3
Kamil Grondys <kamil.grondys@codecool.com>
2020-06-21 10:32:15 +0200

    introducing play_game function
    Adding play_game

--- guessing_game.py
+++ guessing_game.py
@@ -14,9 +14,27 @@ def create_random_list(start, end, count):
 
     return result
 
+def play_game(list):
+    for i in range(GOD_MODE_LIMIT_OF_NUMBERS): 
+        if GOD_MODE:
+            print("god mode", list[i])
+        user_guess = int(input("Enter an integer from 1 to 99: "))
+        while list[i] != user_guess:
+            if user_guess < list[i]:
+                print("guess is low")
+                user_guess = int(input("Enter an integer from 1 to 99: "))
+            elif user_guess > list[i]:
+                print("guess is high")
+                user_guess = int(input("Enter an integer from 1 to 99: "))
+            else:
+                break
+        print("you guessed it!")
+
+
+
 first_list_of_int_to_guess = create_random_list(1, 99, 10)
-
-
+play_game(first_list_of_int_to_guess)
+'''
 for i in range(GOD_MODE_LIMIT_OF_NUMBERS): 
     if GOD_MODE:
         print("god mode", first_list_of_int_to_guess[i])
@@ -33,8 +51,10 @@ for i in range(GOD_MODE_LIMIT_OF_NUMBERS):
         else:
             break
     print("you guessed it!")
-
+'''
 second_list_of_int_to_guess = create_random_list(1, 49, 10)
+
+
 
 for i in range(GOD_MODE_LIMIT_OF_NUMBERS):
     if GOD_MODE:
