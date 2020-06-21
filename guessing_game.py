3d13d46b5b6c53f328daeed2e1633031c3653b38
Kamil Grondys <kamil.grondys@codecool.com>
2020-06-21 10:39:43 +0200

    adding a range to play_game function
    Adding support for custom range to play_game function

--- guessing_game.py
+++ guessing_game.py
@@ -11,18 +11,18 @@ def create_random_list(start, end, count):
 
     return result
 
-def play_game(list):
+def play_game(list, start, end):
     for i in range(GOD_MODE_LIMIT_OF_NUMBERS): 
         if GOD_MODE:
             print("god mode", list[i])
-        user_guess = int(input("Enter an integer from 1 to 99: "))
+        user_guess = int(input(f"Enter an integer from {start} to {end}: "))
         while list[i] != user_guess:
             if user_guess < list[i]:
                 print("guess is low")
-                user_guess = int(input("Enter an integer from 1 to 99: "))
+                user_guess = int(input(f"Enter an integer from {start} to {end}: "))
             elif user_guess > list[i]:
                 print("guess is high")
-                user_guess = int(input("Enter an integer from 1 to 99: "))
+                user_guess = int(input(f"Enter an integer from {start} to {end}: "))
             else:
                 break
         print("you guessed it!")
@@ -30,8 +30,8 @@ def play_game(list):
 
 
 first_list_of_int_to_guess = create_random_list(1, 99, 10)
-play_game(first_list_of_int_to_guess)
+play_game(first_list_of_int_to_guess, 1, 99)
 
 second_list_of_int_to_guess = create_random_list(1, 49, 10)
 
-play_game(second_list_of_int_to_guess)
+play_game(second_list_of_int_to_guess, 1, 49)
